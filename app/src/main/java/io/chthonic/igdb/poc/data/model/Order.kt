2472bd7f5799f34f321fbccb622a103ad6e81ddd
jhavatar <jhdevaal@gmail.com>
2018-09-09 01:24:23 +0200

    ui tweaks

--- app/src/main/java/io/chthonic/igdb/poc/data/model/Order.kt
+++ app/src/main/java/io/chthonic/igdb/poc/data/model/Order.kt
@@ -3,8 +3,17 @@ package io.chthonic.igdb.poc.data.model
 /**
  * Created by jhavatar on 9/9/2018.
  */
-sealed class Order  {
-    object POPULARITY: Order()
-    object USER_REVIEW: Order()
-    object CRITIC_REVIEW: Order()
+sealed class Order(val id: Int)  {
+    companion object {
+        fun fromId(id: Int, fallback: Order): Order {
+            return listOf(POPULARITY, USER_REVIEW, CRITIC_REVIEW).firstOrNull() {
+                it.id == id
+            } ?: fallback
+
+        }
+    }
+
+    object POPULARITY: Order(0)
+    object USER_REVIEW: Order(1)
+    object CRITIC_REVIEW: Order(2)
 }
--- app/src/main/java/io/chthonic/igdb/poc/ui/presenter/MainPresenter.kt
+++ app/src/main/java/io/chthonic/igdb/poc/ui/presenter/MainPresenter.kt
@@ -27,6 +27,8 @@ class MainPresenter(private val kodein: Kodein = App.kodein): BasePresenter<Main
         const val NO_PAGE = -1
         const val FIRST_PAGE = 0
         const val LAST_PAGE = 4
+
+        private val KEY_ORDER = "key_order"
     }
 
     private val igdbService: IgdbService by kodein.lazy.instance<IgdbService>()
@@ -48,18 +50,20 @@ class MainPresenter(private val kodein: Kodein = App.kodein): BasePresenter<Main
 
     override fun onLink(vu: MainVu, inState: Bundle?, args: Bundle) {
         super.onLink(vu, inState, args)
-
-        // force update all ui
-//        vu.updateCalculation(genCalculationViewModel(calculatorService.state).copy(forceSet = true))
-//        vu.updateTickers(genTickerViewModels(igdbService.state.tickers))
-        Timber.d("ui init completed")
-
+        Timber.d("onLink")
+        if (inState?.containsKey(KEY_ORDER) == true) {
+            order = Order.fromId(inState.getInt(KEY_ORDER), Order.POPULARITY)
+        }
         loadingBusy = false
-        subscribeServiceListeners()
         subscribeVuListeners(vu)
         Timber.d("listeners subscribe completed")
 
         vu.updateOrderSelection(order)
+
+        if (vu.getGames().isEmpty()) {
+            lastPage = NO_PAGE
+            canLoadMore = true
+        }
 
         if (lastPage == NO_PAGE) {
             fetchGames()
@@ -67,44 +71,15 @@ class MainPresenter(private val kodein: Kodein = App.kodein): BasePresenter<Main
     }
 
     override fun onUnlink() {
+        Timber.d("onUnlink")
         vu?.hideLoading(true)
         super.onUnlink()
     }
 
-    private fun subscribeServiceListeners() {
-
-//        rxSubs.add(igdbService.observers.tickersChangeObserver
-//                .subscribeOn(Schedulers.computation())
-//                .map {tickerMap: Map<String, Ticker> ->
-//                    genTickerViewModels(tickerMap)
-//                }
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe({tickers: List<TickerViewModel> ->
-//                    Timber.d("tickersChangeObserver success = $tickers")
-//                    vu?.updateTickers(tickers)
-//
-//                }, {it: Throwable ->
-//                    Timber.e(it, "tickersChangeObserver fail")
-//                }))
-//
-//        rxSubs.add(calculatorService.observers.calculationChangeChangeObserver
-//                .subscribeOn(Schedulers.computation())
-//                .map{ calcState: CalculatorState ->
-//                    val exchangeState = igdbService.state
-//                    Pair<CalculationViewModel, List<TickerViewModel>>(genCalculationViewModel(calcState, exchangeState),
-//                            genTickerViewModels(exchangeState.tickers, calcState))
-//                }
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe({viewModels: Pair<CalculationViewModel, List<TickerViewModel>> ->
-//                    Timber.d("calculationChangeChangeObserver success = $viewModels")
-//                    vu?.updateCalculation(viewModels.first)
-//                    vu?.updateTickers(viewModels.second)
-//
-//                }, {it: Throwable ->
-//                    Timber.e(it, "fetchTickerLot fail")
-//                }))
+    override fun onSaveState(outState: Bundle) {
+        outState.putInt(KEY_ORDER, order.id)
+        super.onSaveState(outState)
     }
-
 
     private fun subscribeVuListeners(vu: MainVu) {
         rxSubs.add(vu.refreshObservable
@@ -148,38 +123,6 @@ class MainPresenter(private val kodein: Kodein = App.kodein): BasePresenter<Main
                 }, {t: Throwable ->
                     Timber.e(t, "gameSelectedObservable failed")
                 }))
-
-//        rxSubs.add(vu!!.tickerSelectObservable
-//                .observeOn(Schedulers.computation())
-//                .subscribe({ tickerCode: String ->
-//                    Timber.d("tickerSelectObservable success: $tickerCode")
-//                    calculatorService.setTargetTicker(tickerCode)
-//
-//                }, {t: Throwable ->
-//                    Timber.e(t, "tickerSelectObservable failed")
-//                }))
-//
-//        rxSubs.add(vu!!.leftInputObserver
-//                .observeOn(Schedulers.computation())
-//                .subscribe({amount: String ->
-//                    Timber.d("leftInputObserver success = $amount")
-//                    calculatorService.updateSourceDirectionAndSourceValue(true, amount)
-//
-//                }, {
-//
-//                    Timber.e(it, "leftInputObserver failed")
-//                }))
-//
-//        rxSubs.add(vu!!.rightInputObserver
-//                .observeOn(Schedulers.computation())
-//                .subscribe({amount: String ->
-//                    Timber.d("rightInputObserver success = $amount")
-//                    calculatorService.updateSourceDirectionAndSourceValue(false, amount)
-//
-//                }, {
-//
-//                    Timber.e(it, "rightInputObserver failed")
-//                }))
     }
 
 
@@ -244,98 +187,5 @@ class MainPresenter(private val kodein: Kodein = App.kodein): BasePresenter<Main
     private fun getNextPage(): Int {
         return Math.max(lastPage + 1, FIRST_PAGE)
     }
-//    fun fetchLatestTickers(forceDisplayLoading: Boolean = false) {
-//        Timber.d("fetchLatestTickers: forceDisplayLoading = $forceDisplayLoading")
-//        if (forceDisplayLoading || igdbService.state.tickers.isEmpty()) {
-//            vu?.showLoading()
-//        }
-//
-//        rxSubs.add(igdbService.fetchTickerLot()
-////                .toCompletable()
-//                .subscribeOn(Schedulers.computation())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe({it: List<Ticker> ->
-//                    Timber.d("fetchTickerLot success = $it")
-//                    vu?.hideLoading()
-//
-//                }, {it: Throwable ->
-//                    Timber.e(it, "fetchTickerLot fail")
-//                    vu?.hideLoading()
-//                }))
-//    }
-
-//    fun clearCalculation() {
-////        launch(clearDispatcher) {
-//////            Timber.d("clearCalculation: mainThread = ${Looper.myLooper() == Looper.getMainLooper()}")
-////            calculatorService.clear()
-////        }
-//    }
-
-
-//    private fun genCalculationViewModel(calculatorState: CalculatorState, exchangeState: ExchangeState = igdbService.state): CalculationViewModel {
-//        Timber.d("genCalculationViewModel: mainThread = ${Looper.myLooper() == Looper.getMainLooper()}, calculatorState = $calculatorState")
-//        val leftTicker = CalculatorUtils.getLeftTicker(calculatorState, exchangeState)
-//        val leftTickerViewModel = if (leftTicker != null) {
-//            val leftCurrency = ExchangeUtils.getCurrencyForTicker(leftTicker)
-//            TickerViewModel(leftTicker.code, leftTicker.code,
-//                    TextUtils.formatCurrency(CalculatorUtils.getPrice(leftTicker, calculatorState, exchangeState), leftCurrency?.decimalDigits),
-//                    UiUtils.getCurrencySign(ExchangeUtils.getCurrencyForTicker(leftTicker), ""),
-//                    leftCurrency?.decimalDigits ?: Currency.FIAT_DECIMAL_DIGITS,
-//                    true,
-//                    false,
-//                    calculatorState.leftTickerIsSource,
-//                    TextUtils.getDateTimeString(leftTicker.timestamp))
-//        } else {
-//            null
-//        }
-//        Timber.d("genCalculationViewModel: leftTicker = $leftTicker, \nleftTickerViewModel = $leftTickerViewModel")
-//
-//        val rightTicker = CalculatorUtils.getRightTicker(calculatorState, exchangeState)
-//        val rightTickerViewModel = if (rightTicker != null) {
-//            val rightCurrency = ExchangeUtils.getCurrencyForTicker(rightTicker)
-//            TickerViewModel(rightTicker.code, rightTicker.code,
-//                    TextUtils.formatCurrency(CalculatorUtils.getPrice(rightTicker, calculatorState, exchangeState), rightCurrency?.decimalDigits),
-//                    UiUtils.getCurrencySign(ExchangeUtils.getCurrencyForTicker(rightTicker), ""),
-//                    rightCurrency?.decimalDigits ?: Currency.FIAT_DECIMAL_DIGITS,
-//                    false,
-//                    true,
-//                    !calculatorState.leftTickerIsSource,
-//                    TextUtils.getDateTimeString(rightTicker.timestamp))
-//        } else {
-//            null
-//        }
-//        Timber.d("genCalculationViewModel: rightTicker = $rightTicker, \nrightTickerViewModel = $rightTickerViewModel")
-//
-////        Timber.d("genCalculationViewModel: currency = ${ExchangeUtils.getCurrencyForTicker(ticker!!)}")
-//        return CalculationViewModel(
-//                calculatorState.leftTickerIsSource,
-//                leftTicker = leftTickerViewModel,
-//                rightTicker = rightTickerViewModel,
-//                forceSet = (CalculatorState.getFactoryState() == calculatorState))
-//    }
-//
-//    private fun genTickerViewModels(tickers: Map<String, Ticker>, calcState: CalculatorState = calculatorService.state): List<TickerViewModel> {
-//        Timber.d("genTickerViewModels: mainThread = ${Looper.myLooper() == Looper.getMainLooper()}, tickers = $tickers")
-//        val leftTicker = CalculatorUtils.getLeftTicker(calcState, tickers)
-//        val rightTicker = CalculatorUtils.getRightTicker(calcState, tickers)
-//        return tickers.values
-//                .filter{ ExchangeUtils.isSupportedCurrency(it) }
-//                .sortedBy { it.code }
-//                .map {
-//                    val currency = ExchangeUtils.getCurrencyForTicker(it)
-//                    val isLeft = leftTicker?.code == it.code
-//                    val isRight = rightTicker?.code == it.code
-//                    val isSource = (isLeft && calcState.leftTickerIsSource) || (isRight && !calcState.leftTickerIsSource)
-//                    TickerViewModel(it.code,
-//                            it.code,
-//                            TextUtils.formatCurrency(CalculatorUtils.getPrice(it, calculatorService.state, tickers), currency?.decimalDigits),
-//                            UiUtils.getCurrencySign(ExchangeUtils.getCurrencyForTicker(it), ""),
-//                            currency?.decimalDigits ?: Currency.FIAT_DECIMAL_DIGITS,
-//                            isLeft,
-//                            isRight,
-//                            isSource,
-//                            TextUtils.getDateTimeString(it.timestamp))
-//                }
-//    }
 
 }
