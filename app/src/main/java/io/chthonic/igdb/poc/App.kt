12965886a35d9dd985b25178a0e64fa01dd64869
jhavatar <jhdevaal@gmail.com>
2018-09-09 23:54:47 +0200

    working crashlytics

--- app/src/main/java/io/chthonic/igdb/poc/App.kt
+++ app/src/main/java/io/chthonic/igdb/poc/App.kt
@@ -1,8 +1,12 @@
 package io.chthonic.igdb.poc
 
+import com.crashlytics.android.Crashlytics
 import com.github.salomonbrys.kodein.Kodein
 import io.chthonic.igdb.poc.utils.DebugUtils
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
+
+
 
 /**
  * Created by jhavatar on 3/2/17.
@@ -21,9 +25,10 @@ class App : android.app.Application() {
         if (DebugUtils.dontInitSinceAnalsing(this)) {
             return
         }
+        Fabric.with(this, Crashlytics())
         Timber.plant(if (BuildConfig.DEBUG) Timber.DebugTree() else object: Timber.Tree() {
             override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                // ignore
+                // Nothing
             }
         })
         DebugUtils.install(this)
